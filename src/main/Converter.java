d2f6233a7d5ebc3c93916d2b14f0196e7eacbf25
kileader <kevin.i.leader@gmail.com>
2021-06-11 13:31:20 -0400

    Add Converter class with variables and unfinished while loop

---
+++ src/main/Converter.java
@@ -0,0 +1,23 @@
+package main;
+
+import java.util.Scanner;
+
+public class Converter {
+
+	public static void main(String[] args) {
+		int menuSelection = 0;
+		int exitSelection = 4;
+		Scanner scanner = new Scanner(System.in);
+		
+		while (menuSelection != exitSelection) {
+			System.out.println("Please select an option:");
+			System.out.println("1. Cups to Teaspoons");
+			System.out.println("2. Miles to Kilometers");
+			System.out.println("3. US Gallons to Imperial Gallons");
+			System.out.println("4. Quit");
+			
+		}
+
+	}
+
+}
